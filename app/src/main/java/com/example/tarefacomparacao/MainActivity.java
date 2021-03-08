8369232839cd0ad2ed7b2b0a1821c70c2259ef74
Siff <ricardodsgenari@gmail.com>
2021-03-08 10:22:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tarefacomparacao/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.tarefacomparacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText editPreco1, editPreco2;
+    private TextView textResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editPreco1 = findViewById(R.id.Preco1);
+        editPreco2 = findViewById(R.id.Preco2);
+
+    }
+
+
+    public void calcularMet(View view) {
+    }
+    public Boolean validarcampos(String P1, String P2){
+        Boolean camposvalidados = true;
+        if(P1 == null || P1.equals("")){
+            camposvalidados = false;
+        }
+        else if (P2 == null || P2.equals("")){
+            camposvalidados = false;
+        }
+        return camposvalidados;
+    }
+}
